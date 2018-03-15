41de8ff7f9eb9b4f01038b4647471f338716fa08
paras-aggarwal <31009682+paras-aggarwal@users.noreply.github.com>
2018-03-16 00:41:43 +0530

    Fixing bugs

--- public/init.js
+++ public/init.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 				success: function(data)
 				{
 					console.log('key: '+data);
-					$('#result').html('<p class="bg-success">Alas! Here is your short url<br/><div class="form-group"><div class="input-group"><div class="input-group-addon"><span class="glyphicon glyphicon-link" aria-hidden="true"></span></div><input type="text" class="form-control input-sm"  id="resultKey" name="resultKey" value=http://vipurl.herokuapp.com/'+data+' readonly></div></div></p>');
+					$('#result').html('<p class="bg-success">Alas! Here is your short url<br/><div class="form-group"><div class="input-group"><div class="input-group-addon"><span class="glyphicon glyphicon-link" aria-hidden="true"></span></div><input type="text" class="form-control input-sm"  id="resultKey" name="resultKey" value=http://dockurl.herokuapp.com/'+data+' readonly></div></div></p>');
 
 					// if(data){
 					// 						}else{
