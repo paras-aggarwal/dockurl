0ccf03c780f44c076deb0a59ed3d66d8261334f3
paras-aggarwal <31009682+paras-aggarwal@users.noreply.github.com>
2018-03-17 01:32:59 +0530

    Added redirect

--- index.js
+++ index.js
@@ -2,36 +2,64 @@ var express = require('express');
 
 var app = express();
 
+app.set('views', __dirname + '/views');
 app.use(express.static(__dirname+'/public'));
 
 var bodyParser = require('body-parser')
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
 app.use(bodyParser.json())
 
+// Route for home page
 app.get('/',function(req, res){
 	res.sendFile('shortner.html',{root:__dirname});
 });
 
+// Array to store url and key
+var url_arr = [];
+var key_arr = [];
+
+// Request from init.js to create a short url
 app.post('/short', function(req, res){
+	
+	// Generating a key
 	function makeId(){
 		var text = "";
-    	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    	var combination = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     	for(var i=0; i < 5; i++)
-        	text += possible.charAt(Math.floor(Math.random() * possible.length));
-    	return text;	
+        	text += combination.charAt(Math.floor(Math.random() * combination.length));
+    	return text;
 	}
+
 	var u = req.body.url;
 	var k = req.body.key;
+	
 	if(k == "")
 		k = makeId();
 	var hits = 0;
 	var c = new Date();
+
+	// Adding values in array
+	url_arr.push(u);
+	key_arr.push(k);
+	
 	console.log("Url: "+u+'\nKey: '+k+'\nHits: '+hits+'\nCreated at: '+c+'\n');
 	res.send(k);
+});
+
+// To redirect
+app.get('/:link', function(req, res){
+	var key = req.params.link;
+	console.log("redirect request for: "+key);
+	// Check if the key exist
+	for(var i = 0; i < key_arr.length; i++)
+	{
+		if(key_arr[i] == key)
+        	res.redirect(url_arr[i]);
+	}
+	// If the url is not available in 
+	res.sendFile('error.html',{root:__dirname});
 });
 
 app.get('/*',function(req, res){
