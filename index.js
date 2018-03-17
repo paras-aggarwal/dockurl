290b650fb389f47f75d1653a10b1fc5de5205241
paras-aggarwal <31009682+paras-aggarwal@users.noreply.github.com>
2018-03-17 22:09:29 +0530

    Added DB and Validation

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
 var express = require('express');
-
 var app = express();
+
+// MongoDB Connection
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://paras:paras@ds211029.mlab.com:11029/paras_db');
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+	console.log("Connected To MLab cloud database");
+});
+
+// Schema
+var urlSchema = mongoose.Schema({
+	url: String,
+	key: String,
+	hits: Number,
+	created: String
+});
+
+// Model
+var Url = mongoose.model('Url', urlSchema);
 
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname+'/public'));
 
 var bodyParser = require('body-parser')
-
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.use(bodyParser.json())
 
-// Route for home page
 app.get('/',function(req, res){
 	res.sendFile('shortner.html',{root:__dirname});
 });
 
-// Array to store url and key
-var url_arr = [];
-var key_arr = [];
-
-// Request from init.js to create a short url
 app.post('/short', function(req, res){
-	
-	// Generating a key
 	function makeId(){
 		var text = "";
     	var combination = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -31,35 +41,49 @@ app.post('/short', function(req, res){
         	text += combination.charAt(Math.floor(Math.random() * combination.length));
     	return text;
 	}
-
 	var u = req.body.url;
 	var k = req.body.key;
 	
 	if(k == "")
 		k = makeId();
-	var hits = 0;
+
+	var h = 0;
 	var c = new Date();
 
-	// Adding values in array
-	url_arr.push(u);
-	key_arr.push(k);
-	
-	console.log("Url: "+u+'\nKey: '+k+'\nHits: '+hits+'\nCreated at: '+c+'\n');
+	// Adding values in DB
+	var newUrl = new Url({url: u, key: k, hits: h, created: c});
+	newUrl.save(function (err, testEvent) {
+  		if (err) 
+  			return console.error(err);
+  		console.log("Short Url Created!!");
+	});
+		
+	console.log("Url: "+u+'\nKey: '+k+'\nHits: '+h+'\nCreated at: '+c+'\n');
 	res.send(k);
 });
 
-// To redirect
+app.post('/check',function(req, res){
+	var k = req.body.key;
+    Url.find({key:k},function (err, success) {
+        if(success.length>0)
+          res.send('available');
+        else
+          res.send('unavailable');
+        if (err) 
+        	return console.error(err);
+    });
+});
+
 app.get('/:link', function(req, res){
 	var key = req.params.link;
+	Url.findOne({key:key},function (err, url) {		// find search all and findOne search only for one
+	    console.log(url);   	
+        if(url)
+        	res.redirect(url.url);
+        else if (err)
+        	return console.error(err);	
+    });
 	console.log("redirect request for: "+key);
-	// Check if the key exist
-	for(var i = 0; i < key_arr.length; i++)
-	{
-		if(key_arr[i] == key)
-        	res.redirect(url_arr[i]);
-	}
-	// If the url is not available in 
-	res.sendFile('error.html',{root:__dirname});
 });
 
 app.get('/*',function(req, res){
--- public/js/init.js
+++ public/js/init.js
@@ -1,8 +1,60 @@
 $(document).ready(function(){
+	$('#url').on('change',function(e){
+		var reg = /^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
+		if (reg.test($('#url').val())) { 
+    		$('#valid').html('<font color="green">Looks good!</font>');
+    		$('#button').removeAttr('disabled');
+			return true;
+		}
+		else if(!reg.test($('#url').val()))
+		{
+			$('#valid').html('<font color="red">Enter valid URL!</font>');
+			$('#button').attr('disabled','disabled');
+			return false;
+		}
+	});
+
+	$('#key').on('keyup', function(e){
+		var k = $(this).val();
+		if(k.length == 0)
+			$('#check').html('');
+		else{
+			$.ajax({
+				type: "POST",
+				url: "/check",
+				data: {key: k},
+				success: function(data)
+				{
+					if(data == "available"){
+							$('#check').html('<font color="red">Not Available</font>');
+							$('#button').attr('disabled','disabled');
+					}
+					else{
+						$('#check').html('<font color="green">Available</font>');
+						$('#button').removeAttr('disabled');
+					}
+				},
+				error: function(data)
+				{
+					console.log("App not functioning properly!");
+				}
+			});	
+		}
+	});
+
 	$('#url_shortner').submit(function(e){
 		e.preventDefault();
 		var u = $('#url').val();
 		var k = $('#key').val();
+
+		if(u == "")
+		{
+			$('#valid').html('<font color="red">Enter a valid url!</font>');
+			return;
+		}
+		else
+			$('#valid').html('');
+			
 			$.ajax({
 				type: "POST",
 				url: "/short",
@@ -11,10 +63,11 @@ $(document).ready(function(){
 				{
 					console.log('key: '+data);
 					if(data){
-						$('#result').html('<div class="alert alert-success" role="alert">You have successfully created your short URL, Here is your short URL:</div><div class="form-group"><div class="input-group"><div class="input-group-addon"><span class="glyphicon glyphicon-link" aria-hidden="true"></span></div><input type="text" class="form-control input-sm"  id="resultKey" name="resultKey" value=https://dockurl.herokuapp.com/'+data+' readonly></div></div></p>');
-					}else{
+						$('#result').html('<div class="alert alert-Success" role="alert">You have successfully created your short url, Here is your short url:<br><div class="input-group"><div class="input-group-prepend"><div class="input-group-text"><i class="fas fa-link"></i></div></div><input type="text" class="form-control" value=https://dockurl.herokuapp.com/'+data+' readonly></div></div>');
+					}
+					else{
 						$('#result').html('<div class="alert alert-danger" role="alert">Some error Occured !</div>');
-					}			
+					}
 				},
 				error: function(data)
 				{
